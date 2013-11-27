71649aaa6ac9a5b1f947df265a89e663587ac04e
Michael D Adams <michael.d.adams@gmail.com>
2013-11-27 10:29:18 -0800

    2FA support during installation

--- bin/auth.py
+++ bin/auth.py
@@ -9,11 +9,26 @@ Used by install.sh
 import sys
 import json
 
-response = json.loads( sys.stdin.read() )
+response = sys.stdin.read()
+header_lines, body = response.split( "\r\n\r\n" )
+headers = {}
+for header_line in header_lines.split( "\n" ):
+	try:
+		header, sep, value = header_line.partition( ':' )
+		headers[header.lower()] = value.strip()
+	except:
+		pass
 
-if not response.get( 'id' ) or not response.get( 'token' ):
+
+if 'x-github-otp' in headers and headers['x-github-otp'].startswith( 'required' ):
+	print "OTP"
+	sys.exit( 1 )
+
+body = json.loads( body )
+
+if not body.get( 'id' ) or not body.get( 'token' ):
 	print >> sys.stderr, "Token Request Failed :("
 	sys.exit( 1 )
 
-print "AUTOHUB_ID='%s'" % response['id']
-print "AUTOHUB_TOKEN='%s'" % response['token']
+print "AUTOHUB_ID='%s'" % body['id']
+print "AUTOHUB_TOKEN='%s'" % body['token']
--- install.sh
+++ install.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "$0" )"; pwd )"
+USERNAME=$1
+OTP=$2
 
-read -e -p "GitHub Username: " USERNAME
+if [ -z "$USERNAME" ]
+then
+	read -e -p "GitHub Username: " USERNAME
+fi
 
 exec 3>&1
-eval "$( curl -s -X POST 'https://api.github.com/authorizations' -u "$USERNAME" -d @"$DIR"/payload/auth.json 2> >( ./bin/curl-prompt.py >&3 ) | ./bin/auth.py )"
+if [ -z "$OTP" ]
+then
+	RESULT="$( curl -is -X POST 'https://api.github.com/authorizations' -u "$USERNAME" -d @"$DIR"/payload/auth.json 2> >( ./bin/curl-prompt.py >&3 ) | ./bin/auth.py )"
+else
+	RESULT="$( curl -is -X POST 'https://api.github.com/authorizations' -u "$USERNAME" -H "X-GitHub-OTP: $OTP" -d @"$DIR"/payload/auth.json 2> >( ./bin/curl-prompt.py >&3 ) | ./bin/auth.py )"
+fi
 exec 3>&-
+
+if [ "OTP" = "$RESULT" ]
+then
+	read -e -p "GitHub Two-Factor Authentication Token: " TOKEN
+	"$DIR"/install.sh "$USERNAME" "$TOKEN"
+	exit
+fi
+
+eval "$RESULT"
 
 git config --global AutoHub.id "$AUTOHUB_ID"
 git config --global AutoHub.token "$AUTOHUB_TOKEN"
